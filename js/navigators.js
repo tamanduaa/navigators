5d13a467725bc5ea81f8fabb9be29cd76ecbae53
Steve Minor <stv.mnr@gmail.com>
2016-03-13 12:47:05 -0700

    added navigating behavior

--- js/navigators.js
+++ js/navigators.js
@@ -10,6 +10,7 @@ var cursors;
 var sprite;
 var marker;
 var blocked = false;
+var delay = 0;
 
 var agents = [];
 
@@ -44,6 +45,17 @@ var mainState = {
         marker = game.add.graphics();
         marker.lineStyle(2, 0x000000, 1);
         marker.drawRect(0, 0, 32, 32);
+
+        setupAgents(3);
+
+        findAgentPath({x: 0, y: 0}, {x: 24, y: 14}, agents[0]); 
+        findAgentPath({x: 0, y: 0}, {x: 38, y: 19}, agents[1]); 
+        findAgentPath({x: 0, y: 0}, {x: 15, y: 36}, agents[2]); 
+
+        agents.forEach(function(agent){
+            advanceTween(agent);
+        });
+
     },
     update: function() {
         game.physics.arcade.collide(sprite, layer);
@@ -75,6 +87,16 @@ var mainState = {
             findPathTo(layer.getTileX(marker.x), layer.getTileY(marker.y));
         }
 
+        // march agents along
+        // if(delay++ % 5 == 0){
+        //     agents.forEach(function(agent){
+        //         //update the position of agent
+        //         agent.sprite.x = agent.steps[agent.stepsIndex].x * 32;
+        //         agent.sprite.y = agent.steps[agent.stepsIndex].y * 32;
+        //         ++agent.stepsIndex;
+        //         agent.stepsIndex %= agent.steps.length;
+        //     });
+        // }
     },
     render: function(){
 
@@ -83,11 +105,9 @@ var mainState = {
 
 
 function findPathTo(tilex, tiley) {
-
     pathfinder.setCallbackFunction(function(path) {
         path = path || [];
         for(var i = 0, ilen = path.length; i < ilen; i++) {
-            console.log(path[i].x, path[i].y);
             map.putTile(46, path[i].x, path[i].y);
         }
         blocked = false;
@@ -98,30 +118,48 @@ function findPathTo(tilex, tiley) {
 }
 
 function setupAgents(count){
-    for(var i = 0; i < count; i++){0
+    for(var i = 0; i < count; i++){
         agents.push({
             steps: [],
-            stepsIndex: 0
+            stepsIndex: 0,
+            sprite: game.add.sprite(0, 0, 'car')
         });
     }
 }
 
-function findAgentPath(targetx, targety, agent){
+function findAgentPath(start, end, agent){
     pathfinder.setCallbackFunction(function(path) {
         path = path || [];
         for(var i = 0, ilen = path.length; i < ilen; i++) {
-           agent.steps.push([path[i].x, path[i].y]);
+           agent.steps.push({
+                x: path[i].x * 32,
+                y: path[i].y * 32
+           });
         }
         blocked = false;
     });
 
-    pathfinder.preparePathCalculation([0,0], [targetx,targety]);
+    pathfinder.preparePathCalculation([start.x, start.y], [end.x,end.y]);
     pathfinder.calculatePath();
 }
 
-setupAgents(3);
-findAgentPath(24, 14, agents[0]); 
-findAgentPath(38, 19, agents[1]); 
-findAgentPath(15, 36, agents[2]); 
+function advanceTween(agent){
+    agent.stepsIndex++;
+    agent.stepsIndex %= agent.steps.length;
+    if(agent.stepsIndex == 0){
+        //reset to starting position
+        agent.sprite.x = agent.steps[agent.stepsIndex].x;
+        agent.sprite.y = agent.steps[agent.stepsIndex].y;
+        advanceTween(agent);
+    }else{
+        agent.tween = game.add.tween(agent.sprite);
+        agent.tween.to(agent.steps[agent.stepsIndex], 100, Phaser.Easing.Linear.None);
+        agent.tween.onComplete.add(function(){
+            advanceTween(agent);
+        }, this);
+        agent.tween.start();
+    }
+}
+
 game.state.add('main', mainState);
 game.state.start('main');
